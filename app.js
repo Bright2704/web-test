7f55703e047e1eb24851810ab0553a94c4cc4edf
Bright2704 <thanonchaikantha27@gmail.com>
2024-06-22 02:39:03 +0700

    database

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-function hour2minutes(){
-    input = document.getElementById("inputnumber").value
-    result = input*60
-    str = result + " นาที"
-    console.log(result)
-    
-    document.getElementById("result").innerHTML = str
-}
-
-function minutes2hour(){
-    input = document.getElementById("inputnumber").value
-    result = parseInt(input/60)
-    result2Str = toString(result)
-    result2 = input/60
-    result2 = result2.toFixed(2)
-    index = result2.indexOf(".");
-    result2 = result2.substring(index);
-    result2 = (parseFloat(result2))*60
-    // result = toString(result)
-
-    str = result +" ชั่วโมง " + result2 + " นาที"
-    console.log(result)
-    document.getElementById("result").innerHTML = str
-}
-
+require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
-const port = 8080; // Choose a port number
+const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('welcome to my app service');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
 });
 
+// Test PostgreSQL connection
+pool.query('SELECT NOW()', (err, result) => {
+    if (err) {
+        console.error('Error connecting to PostgreSQL:', err);
+    } else {
+        console.log('Connected to PostgreSQL at:', result.rows[0].now);
+    }
+});
+
+// Express route example
+app.get('/', (req, res) => {
+    // Send both messages after ensuring the PostgreSQL connection check completes
+    pool.query('SELECT NOW()', (err, result) => {
+        if (err) {
+            res.send('Error connecting to PostgreSQL');
+        } else {
+            res.send('Welcome to my app service<br>Connection to PostgreSQL successful');
+        }
+    });
+});
+
+// Start Express server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
